df726dbc83e087dfc89b21142e99e680d2fa76d8
Magdalena Gawlinska <magdalenagawlinska@gmail.com>
2021-02-28 21:18:40 +0100

    Add function to time sorting

--- sorting/handy.py
+++ sorting/handy.py
@@ -13,7 +13,15 @@ __maintainer__ = "Magdalena Gawlinska"
 __email__ = ""
 __status__ = "Prototype"
 
+from timeit import repeat
 
-def time_your_algorithm():
-    raise NotImplementedError
+
+def time_your_sort(algorithm, data, repeat_count):
+    setup_code = f"from __main__ import {algorithm}"
+    stmt = f"{algorithm}({data})"
+    executions = repeat(stmt=stmt, setup=setup_code, repeat=repeat_count, number=1)
+
+    print(f"Algorithm: {algorithm}: Execution times: {executions}. "
+          f"Minimum execution time: {min(executions)}")
+
 
